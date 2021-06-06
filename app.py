4c8eff009be0341f635f6d6e783952ebdcaa4fc8
Yusufu Kamara <yu_kamara@yahoo.com>
2021-06-06 15:55:08 -0700

    Jsonified 'Measurement' and 'Station' query results

--- app.py
+++ app.py
@@ -35,9 +35,6 @@ Base.classes.keys()
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-session = Session(engine)
-
 #################################################
 # Flask Setup
 #################################################
@@ -58,6 +55,58 @@ def home():
         f"/api/v1.0/<start>"
         f"/api/v1.0/<start>/<end>"
     )    
+
+##################################################
+# Jsonifying Precipitation Query Results
+##################################################
+
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all Measurements and close session
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of precipitation
+    precipitation = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict["date"] = date
+        prcp_dict["prcp"] = prcp
+        precipitation.append(prcp_dict)
+
+    return jsonify(precipitation) 
+
+###################################################
+#Jsonifying Station Query Results
+###################################################
+
+@app.route('/api/v1.0/stations')
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all Stations and close session
+    results = session.query(Station.id, Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation).all()
+    session.close()
+
+    stations = []
+    for id, station, name, latitude,longitude,elevation in results:
+        station_dict = {}
+        station_dict["id"] = station
+        station_dict["station"] = station
+        station_dict["name"] = name
+        station_dict["latitude"] = latitude
+        station_dict["longitude"] = longitude
+        station_dict["elevation"] = elevation
+        stations.append(station_dict)
+
+    return jsonify(stations)
+
+
+
 
 if __name__ =="__main__":
     app.run(debug=True)
