4df01cb9727f454ca5f8a8f30fb3f7a5f0541298
Yusufu Kamara <yu_kamara@yahoo.com>
2021-06-06 15:15:51 -0700

    Updated all the required reflections into SQLAlchemy ORM

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
-# Import Flask
+
+#################################################
+# Import dependencies
+#################################################
+import pandas as pd
+import datetime as dt
+import numpy as np
+
+#################################################
+# Python SQL toolkit and Object Relational Mapper
+#################################################
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
 from flask import Flask, jsonify
 
-#Create an app
+
+#################################################
+# Database Setup
+#################################################
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# View all of the classes that automap found
+Base.classes.keys()
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-# Define what to do when a user hits the home page route
+
+#################################################
+# Flask Routes
+#################################################
 @app.route("/")
 def home():
+    """List all available api routes"""
     return (
         f"Welcome to my SQLAlchemy Challenge homepage, availble routes are:<br>"
         f"/api/v1.0/precipitation <br>"
         f"/api/v1.0/stations <br>"
         f"/api/v1.0/tobs <br>"
         f"/api/v1.0/<start>"
-        f"/api/v1.0/<start>"
+        f"/api/v1.0/<start>/<end>"
     )    
 
 if __name__ =="__main__":
